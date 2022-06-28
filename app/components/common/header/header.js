8c796fa51c3f4d34136f964304175ac4a1dccd5a
tovach <red5.dalv@gmail.com>
2022-06-28 09:35:14 +0300

    feat: add basic layout

--- app/components/common/header/header.js
+++
@@ -1,5 +0,0 @@
-const greet = () => {
-  console.log("Hello from header");
-};
-
-export { greet };
--- app/js/_imports.js
+++
@@ -1,4 +0,0 @@
-import { greet } from "../components/common/header/header";
-import { fetchData } from "./utils/fetchData";
-
-export { greet, fetchData };
