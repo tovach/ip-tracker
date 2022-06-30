e9b172f35044eef198f8b2faf53216a74caf89a3
tovach <red5.dalv@gmail.com>
2022-06-30 10:05:20 +0300

    feat: update headers

--- app/js/utils/fetchData.js
+++ app/js/utils/fetchData.js
@@ -1,11 +1,13 @@
 const fetchData = async (url) => {
-  // const headers = new Headers({
-  //   "Accept": "application/json",
-  //   "Content-Type": "application/json",
-  //   "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36",
-  // });
+  const headers = new Headers({
+    Accept: "application/json",
+    "Content-Type": "application/json",
+    "User-Agent":
+      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36",
+    "Access-Control-Allow-Origin": "*",
+  });
   try {
-    const response = await fetch(url);
+    const response = await fetch(url, { mode: "cors", headers });
     if (response.ok) {
       return await response.json();
     } else {
